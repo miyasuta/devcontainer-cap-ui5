b52693ed35ce1510b1a52a2a6c1877bbbe6e3c3e
Mio Yasutake <mio.fujita.01@gmail.com>
2023-08-01 22:16:15 +0200

    ditect dirname

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,11 +1,13 @@
-source docker/.devcontainer/.env
+direName=$(ls | head -n 1)
+source $direName/.devcontainer/.env
 
 echo $GIT_EMAIL
 echo $GIT_USER_NAME
 
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
-git config --global --add safe.directory $PWD/docker/.devcontainer
+git config --global --add safe.directory $PWD/$direName/.devcontainer
 
 echo "GitHub email set to: $GIT_EMAIL"
 echo "GitHub username set to: $GIT_USER_NAME"
+echo "$PWD/$direName/.devcontainer has been added to safe directory"
