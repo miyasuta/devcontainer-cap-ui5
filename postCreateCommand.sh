1d0b7a0ba012e5a16d245070321342365cbe337a
Mio Yasutake <mio.fujita.01@gmail.com>
2023-08-23 22:53:24 +0200

    update postCreateCommand.sh

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -3,11 +3,6 @@
 # 1. Create .env file and maintain the following parameters;
 # GIT_EMAIL=<your Git email>
 # GIT_USER_NAME=<your Git user name>
-#
-# 2. Create kube folder and put config file into it
-
-echo ${containerEnv:DIR_NAME}
-echo $DIR_NAME
 
 source .devcontainer/.env
 
@@ -18,7 +13,3 @@ git config --global --add safe.directory $PWD/.devcontainer
 echo "GitHub email set to: $GIT_EMAIL"
 echo "GitHub username set to: $GIT_USER_NAME"
 echo "$PWD/.devcontainer has been added to safe directory"
-
-# #set KUBECONFIG env variable
-# export KUBECONFIG="$PWD/.devcontainer/kube/config"
-# echo "KUBECONFIG has been set to $KUBECONFIG"
