dc5a2f0d7d3738c48dc159e0e6bff06672447602
Mio Yasutake <mio.fujita.01@gmail.com>
2023-08-23 22:51:50 +0200

    add kubeconfig

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,15 @@ RUN sudo apk update && sudo apk add --no-cache \
     chromium \
     udev \
     ttf-freefont
-ENV CHROME_BIN /usr/bin/chromium-browser
+ENV CHROME_BIN=/usr/bin/chromium-browser
 
 # Install global packages
 RUN  sudo npm install -g yo generator-easy-ui5
 
+# Set kubeconfig
+ARG CURRENT_DIR
+ENV CURRENT_DIR=${CURRENT_DIR}
+ENV KUBECONFIG=${CURRENT_DIR}/kube/config
 
 
 
--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# Create .env file and maintain the following parameters;
+# Settings:
+# 1. Create .env file and maintain the following parameters;
 # GIT_EMAIL=<your Git email>
 # GIT_USER_NAME=<your Git user name>
+#
+# 2. Create kube folder and put config file into it
 
-direName=$(ls | head -n 1)
-source $direName/.devcontainer/.env
+echo ${containerEnv:DIR_NAME}
+echo $DIR_NAME
+
+source .devcontainer/.env
 
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
-git config --global --add safe.directory $PWD/$direName/.devcontainer
+git config --global --add safe.directory $PWD/.devcontainer
 
 echo "GitHub email set to: $GIT_EMAIL"
 echo "GitHub username set to: $GIT_USER_NAME"
-echo "$PWD/$direName/.devcontainer has been added to safe directory"
+echo "$PWD/.devcontainer has been added to safe directory"
+
+# #set KUBECONFIG env variable
+# export KUBECONFIG="$PWD/.devcontainer/kube/config"
+# echo "KUBECONFIG has been set to $KUBECONFIG"
