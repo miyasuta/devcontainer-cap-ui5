e6fba62274e4dba8b1ae64c49df54589dabefbf9
Mio Yasutake <mio.fujita.01@gmail.com>
2023-08-23 23:23:21 +0200

    update devcontainer

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -4,12 +4,12 @@
 # GIT_EMAIL=<your Git email>
 # GIT_USER_NAME=<your Git user name>
 
-source .devcontainer/.env
+source .env
 
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
-git config --global --add safe.directory $PWD/.devcontainer
+git config --global --add safe.directory $PWD
 
 echo "GitHub email set to: $GIT_EMAIL"
 echo "GitHub username set to: $GIT_USER_NAME"
-echo "$PWD/.devcontainer has been added to safe directory"
+echo "$PWD has been added to safe directory"
