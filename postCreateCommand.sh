922549aca9202e7bd1139ee03c4d6080a9627b2b
Mio Yasutake <mio.fujita.01@gmail.com>
2023-09-05 21:44:11 +0200

    set default branch

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -9,6 +9,7 @@ source .env
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
 git config --global --add safe.directory $PWD
+git config --global init.defaultBranch "main"
 
 echo "GitHub email set to: $GIT_EMAIL"
 echo "GitHub username set to: $GIT_USER_NAME"
