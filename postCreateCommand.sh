0a29f51da6ae4607b534e98c84da512cabd2927b
Mio Yasutake <mio.fujita.01@gmail.com>
2023-09-10 22:39:10 +0200

    git config pull.rebase

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -10,6 +10,7 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
 git config --global --add safe.directory $PWD
 git config --global init.defaultBranch "main"
+git config pull.rebase false #do merge
 
 echo "GitHub email set to: $GIT_EMAIL"
 echo "GitHub username set to: $GIT_USER_NAME"
